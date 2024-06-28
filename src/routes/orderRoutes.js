ededf1e452a37ef29fbad7bc1c6450e8ca38376a
Deepumehra <142345479+Deepumehra@users.noreply.github.com>
2024-06-29 03:14:35 +0530

    Update Backend

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
 const authenticate = require('../midleware/authenticate');
-
+const Payment =require('../models/payment.model');
 router.post('',authenticate, orderController.createOrder);
 router.get('/user',authenticate, orderController.getAllUserOrders);
-
+router.get('/getAllOrders',orderController.getAllOrders);
+router.get('/getAllPayments',async (req,res)=>{
+    try{
+        const payments=await Payment.find({})
+        res.json(payments);
+    }catch(error){
+        throw new Error(error.message);
+    }
+});
 module.exports = router;
